218444de6941fabf96b2ca71221cf14725520ab8
Vinicius Gama Santos <114769520+viniciusggama@users.noreply.github.com>
2023-06-15 15:55:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# QUIZ
+ semana sustentavel
---
+++ questions.js
@@ -0,0 +1,93 @@
+export default[
+    {
+        question:"Você sabe qual lâmpada gera menos impacto no meio ambiente?",
+        answers:[
+            {option: "LED",correct: true},
+            {option: "HALOGÉNEO",correct: false},
+            {option: "INCANDESCENTES",correct: false},
+            {option: "FLUORESCENTE COMPACTAS",correct: false},
+        ],
+    },
+    {
+        question:"Qual dos gases abaixo não é conhecido como um dos gases do EFEITO ESTUFA (GEE)?",
+        answers:[
+            {option: "CO2 DIÓXIDO DE CARBONO OU GÁS CARBÔNICO",correct: false},
+            {option: "CH4-METANO",correct: false},
+            {option: "ÓXIDO NITROSOS",correct: false},
+            {option: "OXIGÊNIO",correct: true},
+        ],
+    },
+    {
+        question:"O Brasil é exemplo em reciclagem de:",
+        answers:[
+            {option: "PAPEL PARA USO ESCOLAR",correct: true},
+            {option: "GARRAFAS PLÁSTICAS",correct: false},
+            {option: "LATA DE ALUMÍNIO",correct: false},
+            {option: "VIDROS",correct: false},
+        ],
+    },
+    {
+        question:"Qual cor e a finalidade das lixeiras estão corretas?",
+        answers:[
+            {option: "ROXO-RESÍDUO RADIOATIVO",correct: true},
+            {option: "LARANJA-MADEIRA",correct: false},
+            {option: "VERMELHO-PAPEL E PAPELÃO",correct: false},
+            {option: "BRANCO-RESÍDUOS ORGÂNICOS",correct: false},
+        ],
+    },
+    {
+        question:"Os recursos naturais são finitos, quais citados abaixo é o mais prejudicado atualmente?",
+        answers:[
+            {option: "CARVÃO",correct: false},
+            {option: "PETRÓLEO",correct: false},
+            {option: "ÁGUA",correct: true},
+            {option: "GAS NATURAL",correct: false},
+        ],
+    },
+    {
+        question:"Você sabe em que data é comemorado o Dia Mundial do Meio Ambiente?",
+        answers:[
+            {option: "20/04",correct: false},
+            {option: "09/07",correct: false},
+            {option: "05/06",correct: true},
+            {option: "21/09",correct: false},
+        ],
+    },
+    {
+        question:"No ano de 2000 a ONU criou os ODS (objetivos do desenvolvimento sustentável). Quantos desses objetivos existem?",
+        answers:[
+            {option: "20",correct: false},
+            {option: "15",correct: false},
+            {option: "10",correct: false},
+            {option: "17",correct: true},
+        ],
+    },
+    {
+        question:"A utilização de qual meio de transporte gera mais poluição?",
+        answers:[
+            {option: "CAMINHÃO",correct: false},
+            {option: "CARRO",correct: false},
+            {option: "BARCO",correct: false},
+            {option: "AVIÃO",correct: true},
+        ],
+    },
+    {
+        question:"Quantos quilos de Gás Carbônico um carro novo emite por quilômetro rodado, aproximadamente?",
+        answers:[
+            {option: "1 kg",correct: false},
+            {option: "0,4 kg",correct: true},
+            {option: "1,5 kg",correct: false},
+            {option: "0,1 kg",correct: false},
+        ],
+    },
+
+    {
+        question:"Qual das opções não é uma energia renovável?",
+        answers:[
+            {option: "HÍDRICA",correct: false},
+            {option: "SOLAR",correct: false},
+            {option: "GÁS NATURAL",correct: true},
+            {option: "EÓLICA",correct: false},
+        ],
+    },
+];
